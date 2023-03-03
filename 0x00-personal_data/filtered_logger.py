1e333a282cafc738aab8960458bd242f235c511f
Chalo1996 <emusyoka759@gmail.com>
2023-03-03 16:21:59 +0300

    returns the log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,13 +6,7 @@ import re
 from typing import List
 import logging
 
-PII_FIELDS = (
-    'name',
-    'email',
-    'phone',
-    'ssn',
-    'password',
-)
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
