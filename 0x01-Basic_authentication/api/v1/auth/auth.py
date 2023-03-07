c773b1ca8fb183d50e221d57928fdffa7d57e5db
Chalo1996 <emusyoka759@gmail.com>
2023-03-07 23:16:16 +0300

    Add Authorization header

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,7 +4,6 @@
 from flask import request
 from typing import List, TypeVar
 
-
 class Auth:
     """authentication template."""
 
@@ -21,3 +20,9 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user."""
         return None
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization header."""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
