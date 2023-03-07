bf8d24328cb751fd4047223b747869c642860c39
Chalo1996 <emusyoka759@gmail.com>
2023-03-07 22:37:59 +0300

    defining routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,11 +10,13 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication."""
-        return False
-
-    def authorization_header(self, request=None) -> str:
-        """Authorization header."""
-        return None
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user."""
