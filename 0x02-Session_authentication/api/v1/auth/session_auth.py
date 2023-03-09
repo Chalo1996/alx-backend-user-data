27e7c0cc86af5b3f440ca59433b037bf583cca7f
Chalo1996 <emusyoka759@gmail.com>
2023-03-09 14:42:50 +0300

    create an in-Memory session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,18 @@
 """"session_auth module."""
 
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """SessionAuth class."""
+    user_id_by_session_id = {}
 
-    pass
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id."""
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
