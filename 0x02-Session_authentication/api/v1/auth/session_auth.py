d349102402d094da8a29b5b42af2c0683d62340d
Chalo1996 <emusyoka759@gmail.com>
2023-03-09 22:04:45 +0300

    get user based on their cookie id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 
 import uuid
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -23,3 +24,10 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Overload current_user."""
+        if request is not None:
+            session_id = self.session_cookie(request)
+            user_id = self.user_id_for_session_id(session_id)
+            return User.get(id=user_id)
