9ff123d07de3a8394fa8c1c74e8015f636aa8fe6
Chalo1996 <emusyoka759@gmail.com>
2023-03-11 22:53:08 +0300

    hash a password using bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -15,3 +15,19 @@ def hash_password(password: str) -> bytes:
         bytes: a salted, hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode('UTF8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is_valid: validates that the provided password matches the hashed
+    password.
+
+    Args:
+        hashed_password (bytes): a salted, hashed password, which is a byte
+        string.
+        password (str): a UNICODE string representing the password to hash.
+
+    Returns:
+        bool: True if the provided password matches the hashed password,
+        otherwise False.
+    """
+    return bcrypt.checkpw(password.encode('UTF8'), hashed_password)
