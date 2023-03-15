36a0b9fe3056145c2036b69dee008a94314fb184
Chalo1996 <emusyoka759@gmail.com>
2023-03-15 23:31:54 +0300

    adds a view to authenticate a user

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,7 +2,8 @@
 """session_auth module."""
 
 
-from flask import jsonify, abort, request, make_response
+from api.v1.auth.session_auth import SessionAuth
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models.user import User
 from api.v1.auth.auth import Auth
@@ -21,20 +22,19 @@ def login() -> str:
     """
     email = request.form.get('email')
     password = request.form.get('password')
-
     if not email or email == "":
         return jsonify({"error": "email missing"}), 400
     if not password or password == "":
         return jsonify({"error": "password missing"}), 400
-
-    user_list = User.search({'email': email})
-    user = user_list[0]
+    try:
+        user = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-
-    from api.v1.auth.session_auth import SessionAuth
     session_auth = SessionAuth()
     session_id = session_auth.create_session(user.id)
     response = jsonify(user.to_json())
