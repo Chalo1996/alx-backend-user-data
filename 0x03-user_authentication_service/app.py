f49ae32923d4abbc5bacaf56cd13c49291b36c66
Chalo1996 <emusyoka759@gmail.com>
2023-03-18 00:03:50 +0300

    Implements a "/users" route for registering a new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """App module."""
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +14,18 @@ app = Flask(__name__)
 def Bienvenue() -> str:
     """Return welcome message."""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """Return user message."""
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+    try:
+        AUTH.register_user(email, pwd)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
