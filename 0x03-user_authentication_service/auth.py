d780e9c7e3552fb2c878f23fe7c4ad17690186e8
Chalo1996 <emusyoka759@gmail.com>
2023-03-17 16:54:37 +0300

    Adds a user registration method based on their email and password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Auth module"""
 
+from db import DB
+from user import User
 import bcrypt
 
 
@@ -12,3 +14,32 @@ def _hash_password(password: str) -> bytes:
         str: The hashed password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register_user: Register a user with a hashed password in the database
+
+
+        Args:
+            email (str): User email
+            password (str): User password
+
+        Returns:
+            None
+        """
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError("User {} already exists".format(email))
+        hashpwd = _hash_password(password)
+        new_user = User(email=email, hashed_password=hashpwd)
+        self._db.add_user(email, hashpwd)
+
+        return new_user
